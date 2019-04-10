56dcbaf7fe55bd6875ace030c54a2a096d2affb5
KRMaendo <40663381+KRMaendo@users.noreply.github.com>
2019-04-10 23:14:40 +0200

    slight changes to the server class

--- src/ServerNode.java
+++ src/ServerNode.java
@@ -1,6 +1,23 @@
-import org.w3c.dom.Node;
-
-public class ServerNode <AnyType> {
-private Node next= null;
+/**
+ * We hereby acknowledge that the work handed in is our own original work. If We
+ * have quoted from any other source this information has been correctly referenced.
+ * We also declare that We have read the Namibia University of Science and Technology
+ * Policies on Academic Honesty and Integrity as indicated in our course outline and
+ * the NUST general information and regulations - Yearbook 2018
+ *
+ * @authors:
+ * <Kuizikee Raphael Maendo> <217101461>
+ * <Diina Kalimba><218095597>
+ *<Nkomba Waltraud M><218102062>
+ *
+ *
+ */
+public class ServerNode<AnyType> {
 private AnyType data = null;
+private ServerNode<AnyType> server = null;
+
+public  ServerNode(AnyType data){
+    this.data=data;
+}
+
 }//end class ServerNode
